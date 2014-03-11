0cbddd9b1196dc5766b4a9f7797be2753ea658fd
Chris Dodge <cdodge@edx.org>
2014-03-11 13:58:37 -0400

    remove unused code

--- setup.py
+++ setup.py
@@ -2,20 +2,7 @@
 
 # Imports ###########################################################
 
-import os
 from setuptools import setup
-
-
-# Functions #########################################################
-
-def package_data(pkg, root):
-    """Generic function to find package_data for `pkg` under `root`."""
-    data = []
-    for dirname, _, files in os.walk(os.path.join(pkg, root)):
-        for fname in files:
-            data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
-
-    return {pkg: data}
 
 
 # Main ##############################################################
