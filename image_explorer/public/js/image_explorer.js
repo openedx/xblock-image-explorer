03638090d9e5fbacf5111dbed19cd85a643069f1
E. Kolpakov <eugeny.kolpakov@gmail.com>
2014-09-09 10:22:59 +0400

    Added ability to force reveal side in xblock markup

--- image_explorer/public/js/image_explorer.js
+++ image_explorer/public/js/image_explorer.js
@@ -10,6 +10,29 @@ function ImageExplorerBlock(runtime, element) {
       });
     }
 
+    function setRevealPosition(target, reveal) {
+      var reveal_side = reveal.data('side');
+      var reveal_width = reveal.outerWidth();
+      var target_position_left = target.position().left;
+      var hotspot_image_width = target.outerWidth();
+
+      var image_width = reveal.parents('.image-explorer-hotspot').siblings('.image-explorer-background').outerWidth();
+
+      if (reveal_side !== 'left' && reveal_side !== 'right') {
+          /* do some calculations with regards which side of the hotspot to open the feedback on */
+          if ((target_position_left + reveal_width > image_width) &&
+              (target_position_left - reveal_width - hotspot_image_width > 0)) {
+              reveal_side = 'left';
+          }
+          else {
+              reveal_side = 'right';
+          }
+      }
+      if (reveal_side === 'left') {
+          reveal.css('margin-left', '-' + (reveal_width + hotspot_image_width) + 'px');
+      }
+    }
+
     /* reveal feedback action */
     $(element).find('.image-explorer-hotspot').bind('click', function(eventObj) {
       eventObj.preventDefault();
@@ -17,18 +40,10 @@ function ImageExplorerBlock(runtime, element) {
       $(element).find('.image-explorer-hotspot-reveal').css('display', 'none');
 
       var target = $(eventObj.currentTarget);
-      var target_position_left = target.position().left;
-      var hotspot_image_width = target.outerWidth();
       var reveal = target.find('.image-explorer-hotspot-reveal');
-      var reveal_width = reveal.outerWidth();
-      var parent_wrapper = reveal.parents('.image-explorer-hotspot');
-      var image_element = parent_wrapper.siblings('.image-explorer-background');
-      var image_width = image_element.outerWidth();
 
-      /* do some calculations with regards which side of the hotspot to open the feedback on */
-      if ((target_position_left + reveal_width > image_width) && (target_position_left - reveal_width - hotspot_image_width > 0)) {
-        reveal.css('margin-left', '-' + (reveal_width + hotspot_image_width) + 'px');
-      }
+      setRevealPosition(target, reveal);
+
       reveal.css('display', 'block');
       hotspot_opened_at = new Date().getTime();
       publish_event({
