cf701addd81f2d2ad749492b4cdb3f7351e25efd
dragonfi <david.gabor.bodor@gmail.com>
2014-10-09 18:31:50 +0200

    Extract resource loading into xblock-utils

--- image_explorer/utils.py
+++ image_explorer/utils.py
@@ -8,24 +8,10 @@ import pkg_resources
 
 from django.template import Context, Template
 
+from xblockutils.resources import ResourceLoader
+
 log = logging.getLogger(__name__)
-
-
-def load_resource(resource_path):
-    """
-    Gets the content of a resource
-    """
-    resource_content = pkg_resources.resource_string(__name__, resource_path)
-    return unicode(resource_content)
-
-
-def render_template(template_path, context={}):
-    """
-    Evaluate a template by resource path, applying the provided context
-    """
-    template_str = load_resource(template_path)
-    template = Template(template_str)
-    return template.render(Context(context))
+loader = ResourceLoader(__name__)
 
 
 class AttrDict(dict):
