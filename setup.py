7253e0cab73fb985d1fdaa678e9c52b35a11d714
Jillian Vogel <jill@opencraft.com>
2018-05-08 12:37:38 +0930

    Use xblock-utils@v1.1.0
    and bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def package_data(pkg, root_list):
 
 setup(
     name='xblock-image-explorer',
-    version='0.9.0',
+    version='1.0.0',
     description='XBlock - Image Explorer',
     packages=['image_explorer'],
     install_requires=[
