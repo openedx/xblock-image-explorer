6ae5dba5a74f7bf112d8b62eed31241cb5023401
Josue Balandrano Coronel <jbc@rmcomplexity.com>
2019-03-18 17:11:42 -0500

    Compact statement.

--- image_explorer/image_explorer.py
+++ image_explorer/image_explorer.py
@@ -373,9 +373,7 @@ class ImageExplorerBlock(XBlock):  # pylint: disable=no-init
             if not hotspot.y.endswith('%'):
                 hotspot.y += 'px'  # px is deprecated as it is not responsive
 
-            hotspot.visited = False
-            if hotspot.item_id in self.opened_hotspots:
-                hotspot.visited = True
+            hotspot.visited = hotspot.item_id in self.opened_hotspots:
 
             hotspots.append(hotspot)
 
