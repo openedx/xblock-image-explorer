97e3c8372a6db853db6913790f29e0a2ee17dc5d
hussnain <sent.hussnain@gmail.com>
2020-02-06 18:41:59 +0500

    MCKIN-12927 | version updated

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def package_data(pkg, root_list):
 
 setup(
     name='xblock-image-explorer',
-    version='1.1.12',
+    version='1.1.13',
     description='XBlock - Image Explorer',
     packages=['image_explorer'],
     install_requires=[
