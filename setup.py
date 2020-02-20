b78758ad4e82e35cb6df97d263efd83bface62d8
Muhammad Usman <43761905+musmanmalik@users.noreply.github.com>
2020-02-20 14:16:24 +0500

    MCKIN-13279 Dialog content made reader accessible (#78)
    * IE fix for Chrome and Internet explorer

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def package_data(pkg, root_list):
 
 setup(
     name='xblock-image-explorer',
-    version='1.1.15',
+    version='1.1.17',
     description='XBlock - Image Explorer',
     packages=['image_explorer'],
     install_requires=[
