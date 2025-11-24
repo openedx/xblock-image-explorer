ed994b2a115b17c9020cdee69449572e5bc94df7
waleed.mujahid <waleed.mujahid@arbisoft.com>
2025-11-24 13:49:01 +0500

    fix: render buttons in LMS
    These buttons need to be loaded in both lms and studio
    explanation:
    https://github.com/openedx/xblock-image-explorer/pull/83#issuecomment-741960809

--- image_explorer/public/js/image_explorer.js
+++ image_explorer/public/js/image_explorer.js
@@ -1,12 +1,11 @@
 function applyHotspotButtonStyle(element, data) {
+  console.log("data", data['hotspot_image']);
    $(".image-explorer-wrapper button.image-explorer-hotspot", element).css("background", "url("+data['hotspot_image']+") no-repeat scroll 0 0 rgba(0, 0, 0, 0)");
 }
 
 function ImageExplorerBlock(runtime, element, data) {
 
-    if (data['authoring_view'] === 'true') {
-        applyHotspotButtonStyle(element, data);
-    }
+    applyHotspotButtonStyle(element, data);
     var hotspot_opened_at = null;
     var active_feedback = null;
     // Holds a reference to YouTube API Player objects.
